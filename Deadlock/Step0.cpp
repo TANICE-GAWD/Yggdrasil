1ce92d12746bddab9a2562526857d31c6a2247e2
TANICE-GAWD <2006princesharma@gmail.com>
2026-01-29 11:37:28 +0530

    spawn_children

--- Deadlock/Step0.cpp
+++ Deadlock/Step0.cpp
@@ -1,22 +1,40 @@
 #include <iostream>
 #include <unistd.h>
 using namespace std;
-#define INFINITE_FORK 1000
+#define INFINITE_FORK 10
 int main() {
     pid_t p_id = fork();
-    
-    
+    int gen = 0;
+    int spawn_children = 0;
+    Spawning_forks(p_id, gen, spawn_children);
 
     return 0;
 }
 
-pid_t Spawning_forks(pid_t p_id){
+pid_t Spawning_forks(pid_t p_id, int gen, int spawn_children){
+    // for (int i=0;i<INFINITE_FORK;i++){
+    //     pid_t p_id = fork();
+    //     pid_t parent = p_id;
+    //     int parent_gen = gen;
+        // if(p_id){
+        //     Spawning_forks(p_id, gen);
+        // }
+        // else if(!p_id){
+        //     Spawning_forks(p_id, gen);
+        // }
+    // }
     for (int i=0;i<INFINITE_FORK;i++){
-        if(fork){
-            Spawning_forks(p_id);
-        }
-        else if(!fork){
-            Spawning_forks(p_id)
+        if(spawn_children) return;
+        else{
+            spawn_children = 1;
+            p_id = fork();
+            if(p_id){
+                Spawning_forks(p_id, gen, spawn_children);
+            }
+            else if(!p_id){
+                Spawning_forks(p_id, gen, spawn_children);
+            }
+            
         }
     }
 }
