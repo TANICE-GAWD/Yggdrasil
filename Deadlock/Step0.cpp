99953b0f96d63fb4077961225c1a66680268aab9
TANICE-GAWD <2006princesharma@gmail.com>
2026-01-29 11:17:14 +0530

    Infinite fork

--- Deadlock/Step0.cpp
+++ Deadlock/Step0.cpp
@@ -1,1 +1,22 @@
 #include <iostream>
+#include <unistd.h>
+using namespace std;
+#define INFINITE_FORK 1000
+int main() {
+    pid_t p_id = fork();
+    
+    
+
+    return 0;
+}
+
+pid_t Spawning_forks(pid_t p_id){
+    for (int i=0;i<INFINITE_FORK;i++){
+        if(fork){
+            Spawning_forks(p_id);
+        }
+        else if(!fork){
+            Spawning_forks(p_id)
+        }
+    }
+}
