4f3cbbb98827f295b8e6acd77c0470be8ce050c5
TANICE-GAWD <2006princesharma@gmail.com>
2026-01-31 23:49:45 +0530

    Step sigterm

--- Deadlock/Step1.cpp
+++ Deadlock/Step1.cpp
@@ -1,15 +1,21 @@
 // signal blockers inside PID1 controller
 // with notes for myself
 #include <signal.h>
+#include <cstdlib>
+#include <cstdio>
 void blockSignals(){
     sigset_t sig_set; //imma use ts to keep all my blocker signals
     sigemptyset(&sig_set);
 
     sigaddset(&sig_set,SIGTERM); //used ts to add the SIGTERM into the set
 
-    pthread_sigmask(SIG_BLOCK, &sig_set, NULL);
+    // pthread_sigmask(SIG_BLOCK, &sig_set, NULL);
     // above line blocked ts sig
+    // never SIG_UNBLOCK this hehehehehe
 
-    
+    if(sigprocmask(SIG_BLOCK,&sig_set, nullptr) == -1){
+        perror("NOOOOO why T_T");
+        exit(EXIT_FAILURE);
+    }
 
 }
