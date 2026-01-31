980b0f4d25965ca0e83e154e98d6130a9b13c58d
TANICE-GAWD <2006princesharma@gmail.com>
2026-01-31 21:45:58 +0530

    Generation depth control

--- Deadlock/Step0.cpp
+++ Deadlock/Step0.cpp
@@ -11,30 +11,29 @@ int main() {
     return 0;
 }
 
-void Spawning_forks(pid_t p_id, int gen, int spawn_children){
-    // for (int i=0;i<INFINITE_FORK;i++){
-    //     pid_t p_id = fork();
-    //     pid_t parent = p_id;
-    //     int parent_gen = gen;
-        // if(p_id){
-        //     Spawning_forks(p_id, gen);
-        // }
-        // else if(!p_id){
-        //     Spawning_forks(p_id, gen);
-        // }
-    // }
-        if(spawn_children) return;
-        else{
-            spawn_children = 1;
-            p_id = fork();
-            int parent_gen = gen;
-            if(p_id == 0){
-                gen = parent_gen + 1;
-                Spawning_forks(p_id, gen, spawn_children);
-            }
-            else{
-                cout<<"Spawn Failed of gen = " << gen;
-            }
+void Spawning_forks(int gen){
+    // // for (int i=0;i<INFINITE_FORK;i++){
+    // //     pid_t p_id = fork();
+    // //     pid_t parent = p_id;
+    // //     int parent_gen = gen;
+    //     // if(p_id){
+    //     //     Spawning_forks(p_id, gen);
+    //     // }
+    //     // else if(!p_id){
+    //     //     Spawning_forks(p_id, gen);
+    //     // }
+    // // }
+    //     if(spawn_children) return;
+    //     else{
+    //         spawn_children = 1;
+    //         p_id = fork();
+    //         int parent_gen = gen;
+    //         if(p_id == 0){
+    //             gen = parent_gen + 1;
+    //             Spawning_forks(p_id, gen, spawn_children);
+    //         }
             
-    }
+    // }
+
+
 }
