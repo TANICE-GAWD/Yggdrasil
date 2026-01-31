6b6aa1902f099dbcb4fe6be66c4eb118d84f1420
TANICE-GAWD <2006princesharma@gmail.com>
2026-02-01 00:07:26 +0530

    my dumbahh forgot : T_T

--- Deadlock/Step0.cpp
+++ Deadlock/Step0.cpp
@@ -1,40 +1,44 @@
+// signal blockers inside PID1 controller
+// with notes for myself
 #include <iostream>
+#include <sys/types.h>
 #include <unistd.h>
-using namespace std;
-#define INFINITE_FORK 10
-int main() {
-    pid_t p_id = fork();
-    int gen = 0;
-    Spawning_forks(gen);
-
-    return 0;
-}
-
-void Spawning_forks(int gen){
-    // // for (int i=0;i<INFINITE_FORK;i++){
-    // //     pid_t p_id = fork();
-    // //     pid_t parent = p_id;
-    // //     int parent_gen = gen;
-    //     // if(p_id){
-    //     //     Spawning_forks(p_id, gen);
-    //     // }
-    //     // else if(!p_id){
-    //     //     Spawning_forks(p_id, gen);
-    //     // }
-    // // }
-    //     if(spawn_children) return;
-    //     else{
-    //         spawn_children = 1;
-    //         p_id = fork();
-    //         int parent_gen = gen;
-    //         if(p_id == 0){
-    //             gen = parent_gen + 1;
-    //             Spawning_forks(p_id, gen, spawn_children);
-    //         }
-            
-    // }
-
-    for(int i=0;i<gen && )
+#include <signal.h>
+#include <cstdlib>
+#include <sys/wait.h>
+#include <thread>
+#include <chrono>
 
 
-}
+
+class PID1ctrller{
+    private:
+    int max;
+    int cur_process;
+    bool pls_pls_pls_stop;
+
+    public:
+    PID1ctrller(int max):
+                        max(max),
+                        cur_process(1),
+                        pls_pls_pls_stop(false){}
+    
+    void blockSignals(){
+        sigset_t sig_set; //imma use ts to keep all my blocker signals
+        sigemptyset(&sig_set);
+
+        sigaddset(&sig_set,SIGTERM); //used ts to add the SIGTERM into the set
+
+        // pthread_sigmask(SIG_BLOCK, &sig_set, NULL);
+        // above line blocked ts sig
+        // never SIG_UNBLOCK this hehehehehe
+
+        if(sigprocmask(SIG_BLOCK,&sig_set, nullptr) == -1){
+            perror("NOOOOO why T_T");
+            exit(EXIT_FAILURE);
+        }
+
+        sigaddset(&sig_set, SIGINT); //to block ctrl+C
+        sigprocmask(SIG_BLOCK, &sig_set, nullptr);
+    }
+};
