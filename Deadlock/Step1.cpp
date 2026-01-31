bef8321197f10a018074368fdc0e90d3bcab5589
TANICE-GAWD <2006princesharma@gmail.com>
2026-01-31 23:58:09 +0530

    my dumbahh forgot : T_T

--- Deadlock/Step1.cpp
+++ Deadlock/Step1.cpp
@@ -23,30 +23,22 @@ class PID1ctrller{
                         cur_process(1),
                         pls_pls_pls_stop(false){}
     
-    
+    void blockSignals(){
+        sigset_t sig_set; //imma use ts to keep all my blocker signals
+        sigemptyset(&sig_set);
 
+        sigaddset(&sig_set,SIGTERM); //used ts to add the SIGTERM into the set
 
+        // pthread_sigmask(SIG_BLOCK, &sig_set, NULL);
+        // above line blocked ts sig
+        // never SIG_UNBLOCK this hehehehehe
 
+        if(sigprocmask(SIG_BLOCK,&sig_set, nullptr) == -1){
+            perror("NOOOOO why T_T");
+            exit(EXIT_FAILURE);
+        }
 
-}
-
-
-void blockSignals(){
-    sigset_t sig_set; //imma use ts to keep all my blocker signals
-    sigemptyset(&sig_set);
-
-    sigaddset(&sig_set,SIGTERM); //used ts to add the SIGTERM into the set
-
-    // pthread_sigmask(SIG_BLOCK, &sig_set, NULL);
-    // above line blocked ts sig
-    // never SIG_UNBLOCK this hehehehehe
-
-    if(sigprocmask(SIG_BLOCK,&sig_set, nullptr) == -1){
-        perror("NOOOOO why T_T");
-        exit(EXIT_FAILURE);
+        sigaddset(&sig_set, SIGINT); //to block ctrl+C
+        sigprocmask(SIG_BLOCK, &sig_set, nullptr);
     }
-
-    sigaddset(&sig_set, SIGINT); //to block ctrl+C
-    sigprocmask(SIG_BLOCK, &sig_set, nullptr);
-
-}
+};
